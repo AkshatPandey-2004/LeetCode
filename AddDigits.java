b417bdbb371b801af301e3e715cbed17256d7c38
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-01-04 21:26:48 +0530

    258

---
+++ AddDigits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int addDigits(int num) {
+       /*int n=Integer.toString(num).length();
+       int sum=0;
+       if(num>=0 && num<=9){
+           return num;
+       }
+       for(int i=0;i<n;i++){
+           sum+=num%10;
+           num/=10;
+           if(sum/10==1){
+               sum+=sum/10;
+               sum%=10;
+           }
+       }
+       return sum; 
+    }*/
+    if(num%9!=0 || num==0){
+        return num%9;
+    }
+    else{
+        return 9;
+    }
+}
+}
