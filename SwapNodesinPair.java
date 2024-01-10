8529068e5c0202f14d284411995e849f25f46d85
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-01-10 23:27:25 +0530

    24

---
+++ SwapNodesinPair.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode p=head.next;
+        head.next=p.next;
+        p.next=head;
+        head=p;
+        p=p.next;
+        try{
+            while(p.next!=null || p.next.next!=null ){
+                ListNode q=p.next;
+                p.next=p.next.next;
+                p=p.next;
+                if(q.next!=null)
+                q.next=q.next.next;
+                if(p.next!=null)
+                p.next=q;
+            }
+        }
+        catch(NullPointerException e){
+            System.out.print(e);
+        }
+        return head;
+    }
+}
